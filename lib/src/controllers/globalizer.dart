604d7d551ad631ad7ab7eed6794f43a86725e4ef
Fisayo <obilaja@gmail.com>
2022-03-03 14:58:05 +0100

    refact: Optimized for pub.dev publishing

--- lib/src/controllers/globalizer.dart
+++ lib/src/controllers/globalizer.dart
@@ -6,13 +6,13 @@ import 'package:get/get_state_manager/get_state_manager.dart';
 ///This class contains a method that makes non-service variables global
 //well, I could have easily made this a static class, it would have still held the state globally.
 class GlobalizerController extends GetxController {
-  late PaymentConfig paymentConfig;
+  PaymentConfig? paymentConfig;
   static bool isTest = false;
 
   globalizerMethod(param) {
     paymentConfig = param;
-    isTest = isAPIkeyTest(paymentConfig.publicKey);
+    isTest = isAPIkeyTest(paymentConfig!.publicKey);
     debugPrint(
-        'Is publicKey a TestKey ? : ${isAPIkeyTest(paymentConfig.publicKey)}');
+        'Is publicKey a TestKey ? : ${isAPIkeyTest(paymentConfig!.publicKey)}');
   }
 }
