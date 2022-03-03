a23b6f3951001b39b1b863e3f180e6251722d214
Fisayo <obilaja@gmail.com>
2022-03-03 14:44:10 +0100

    refact: Optimized for pub.dev publishing

--- lib/src/utils/alert.dart
+++ lib/src/utils/alert.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../../coinforbarter_sdk.dart';
 
 showAlert(context) {
-  final ServiceController _serviceController = Get.find();
+  final ServiceController _serviceController = Get.put(ServiceController());
   final ListeningToPaymentController listening_to_payment_controller =
       Get.find();
   return showDialog<void>(
