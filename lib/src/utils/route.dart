b5f7de009a0b2e3b78b82fc690fba91755fddf1d
Fisayo <obilaja@gmail.com>
2022-03-23 18:46:48 +0100

    added sensitive keys ti gitginore

--- lib/src/utils/route.dart
+++ lib/src/utils/route.dart
@@ -1,15 +1,14 @@
 // ignore_for_file: non_constant_identifier_names
 
+import 'keys.dart';
+
 class Util {
   static String qrCode =
       'https://www.qr-code-generator.com/wp-content/themes/qr/new_structure/assets/media/images/api_page/qrcodes/bw/Api_page_-_QR-Code-Generator_com-1.png?';
   static String END_POINT = 'https://api.coinforbarter.com/v1';
   // static String END_POINT = 'https://coinforbarter-api.herokuapp.com/v1';
 
-  // static String PUBLIC_KEY =
-  //     "C4B_TEST-ZDOwlonfkWfjeI4UNGuRaUBhUqZID1630870497617";
-  static String PUBLIC_KEY =
-      "C4B_TEST-xMxiwbIsK1632761320337oB8yYtGe3K8wBXFPPhZQ";
+  static String PUBLIC_KEY = SensitiveKeys.publicKeyTest;
 }
 
 class QRcode {
